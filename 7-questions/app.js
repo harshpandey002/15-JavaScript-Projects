f9649f34cbe40a2b147e7df78922aabb85903ba0
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-04-01 00:01:20 +0530

    Questions

--- 7-questions/app.js
+++ 7-questions/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+
+// TRAVERSING THE DOM
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     console.log(e.currentTarget.parentElement.parentElement);
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
